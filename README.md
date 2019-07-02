339bc1561807e17e761bb834b006cc98183f5c30
Gaurav <gaurav.2897@gmail.com>
2019-07-02 14:10:34 +0530

    First Commit:  ReadServer and Construct dictionary DYnamically

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PythonDemo
-Demo Repo about Python
+#Demo Repo about Python
+# Python program to read servername and ip address from file and then create a dictionary
+#read the file from etc directory line by line and then split it store it in list and then create a dictionary
---
+++ readServer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+file=open("/users/apple/Downloads/etc/server.cfg")
+
+
+#first open file
+#read all lines or read line by line
+serverDict={}
+lines=file.readlines()
+print (type(lines))
+for line in lines:
+	print (line)
+	keyValue=line.split(" ")
+	keyValue[0].rstrip()
+	keyValue[1].rstrip()
+	print(keyValue[0])
+	print(keyValue[1])
+	print("KeyValue[0] is",keyValue[0])
+	serverDict[keyValue[0]]=keyValue[1]
+
+print("___________")
+print(serverDict.items())
+print("-------")
+for key,val in serverDict.items():
+	print(key)
+	print(val)
+
+
+print (serverDict['server4'])
+
+
+
